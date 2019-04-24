0f740afbd1dc641e1a0102b453b16b33c7ba07de
Unknown <villajohnny@gmail.com>
2019-04-24 11:23:39 -0500

    Agregado Controller

--- src/main/java/com/medico/app/web/models/services/IRecetaService.java
+++ src/main/java/com/medico/app/web/models/services/IRecetaService.java
@@ -6,6 +6,7 @@ import com.medico.app.web.models.entities.Receta;
 
 
 public interface IRecetaService {
+//@Transactional en caso de hacer con el primer metodo
 
 	public void save(Receta receta);
 	
